444411446641e040346c8223a604706249e746db
Stanislav Miednikov <luxshd@gmail.com>
2020-11-24 21:26:03 +0200

    fix js hw21

--- assets/js/homework21/main.js
+++ assets/js/homework21/main.js
@@ -1,27 +1,40 @@
 function getBodyIndex(){
-    let height = userHeight.value;
-    let mass = userMass.value;
+    
+    
     let descriprion;
 
-    let index = mass / (height ** 2);
-    index = Math.round(index);
+    if(userHeight.validity.valid && userMass.validity.valid){
+        let height = userHeight.value;
+        let mass = userMass.value;
 
-    if(index <= 16){
-        descriprion = 'Выраженный дефицит массы тела';
-    } else if(index <= 18.5){
-        descriprion = 'Недостаточная (дефицит) масса тела';
-    } else if(index <= 25){
-        descriprion = 'Норма';
-    } else if(index <= 30){
-        descriprion = 'Избыточная масса тела (предожирение)';
-    } else if(index <= 35){
-        descriprion = 'Ожирение';
-    } else if(index <= 40){
-        descriprion = 'Ожирение резкое';
-    } else if(index > 40){
-        descriprion = 'Очень резкое ожирение';
+        let index = mass / (height ** 2);
+        index = Math.round(index);
+
+        if(index <= 16){
+            descriprion = 'Выраженный дефицит массы тела';
+        } else if(index <= 18.5){
+            descriprion = 'Недостаточная (дефицит) масса тела';
+        } else if(index <= 25){
+            descriprion = 'Норма';
+        } else if(index <= 30){
+            descriprion = 'Избыточная масса тела (предожирение)';
+        } else if(index <= 35){
+            descriprion = 'Ожирение';
+        } else if(index <= 40){
+            descriprion = 'Ожирение резкое';
+        } else if(index > 40){
+            descriprion = 'Очень резкое ожирение';
+        }
+
+        indexValue.innerHTML = index;
+        indexDescription.innerHTML = descriprion;
+    } else {
+        console.log('invalid data');
     }
+    
+    
 
-    indexValue.innerHTML = index;
-    indexDescription.innerHTML = descriprion;
+    
+
+    
 }
